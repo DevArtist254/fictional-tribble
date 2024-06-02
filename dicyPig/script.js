79f08be00e92612c4a7cb76f87a5cb448ba371e2
DevArtist254 <kevinwasonga415@outlook.com>
2024-06-02 08:20:11 +0000

    fn

--- dicyPig/script.js
+++ dicyPig/script.js
@@ -1,32 +1,70 @@
-///////////////////////////////////////////////
-//User rolls dice
+const activeScore = document.querySelector(".score");
+const hold = document.querySelector(".holdScoreGame");
+const rollDice = document.querySelector(".rollDice");
+const player1 = document.querySelector(".player1");
+const player2 = document.querySelector(".player2");
+const ps1 = document.querySelector(".player1Score");
+const ps2 = document.querySelector(".player2Score");
 
-/**
- * Generates random dice roll
- * Display dice roll 
- * if its 1 switch switch player
- * add dice roll to the score board and the player can go again
- */
+const imgP1 = document.querySelector(".player1Dice");
+const imgP2 = document.querySelector(".player2Dice");
 
-const player1 = {
-    score: 0,
-    isActive: false
+const ps = {
+  playerScore1: 0,
+  playerScore2: 0,
+  score: 0
 };
 
-const player2 = {
-    score: 0,
-    isActive: false
-};
+rollDice.addEventListener("click", (e) => {
+  e.preventDefault();
+  let count = 0;
+  
+  if (generatesRanDiceRoll() !== 1) {
+
+    count = generatesRanDiceRoll();
+    ps.score += count;
+    activeScore.innerHTML = ps.score;
+
+    dice(count);
+  } else {
+    displayScore(ps.score);
+    switchPlayer();
+    ps.score = 0;
+  }
+})
+
+hold.addEventListener("click", (e) => {
+  e.preventDefault();
+  displayScore(ps.score);
+  switchPlayer();
+  ps.score = 0;
+})
 
 
-function generatesRanDiceRoll() {
-    return Math.floor(Math.random() * 6 + 1);;
+function displayScore (score) {
+  if (player1.classList.contains("active")) {
+    ps.playerScore1 += score;
+    ps1.innerHTML = ps.playerScore1;
+  }
+  if (player2.classList.contains("active")) {
+    ps.playerScore2 += score;
+    ps2.innerHTML = ps.playerScore2;
+  }
 }
 
-if (generatesRanDiceRoll() !== 1) {
-    player1.score += generatesRanDiceRoll();   
-    console.log(player1.score);
-} else {
-    console.log("Players current score: " + player1.score);
-    console.log("Switch player");
+function switchPlayer() {
+    player1.classList.toggle("active");
+    player2.classList.toggle("active");
+}
+
+function dice(count) {
+  if (player1.classList.contains("active")) {
+    imgP1.src = `./assests/dice-${count}.png`;
+  } else if (player2.classList.contains("active")) {
+    imgP2.src = `./assests/dice-${count}.png`;
+  }
+}
+
+ function generatesRanDiceRoll() {
+     return Math.floor(Math.random() * 6 + 1);;
 }
