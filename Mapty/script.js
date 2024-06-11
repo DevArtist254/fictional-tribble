9ab91846a55c4cceb1b265d6369c6152fc71052b
DevArtist254 <kevinwasonga415@outlook.com>
2024-06-11 17:09:12 +0000

    Mapty-architecture

---
+++ Mapty/script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+class Workout {
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+    clicks = 0;
+
+    constructor(coords, distance, duration){
+        this.coords = coords;
+        this.distance = distance;
+        this.duration = duration;
+    }
+
+    _setDescription(){
+        const months = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ];
+
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
+
+    click(){
+        this.clicks++;
+    }
+}
+
+class Running extends Workout{
+    type  = 'running';
+
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+        this._setDescription();
+    }
+
+    calcPace() {
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout {
+    type = 'cycling';
+
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+        this._setDescription();
+    }
+
+    calcSpeed() {
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+
+const run = new Running([24, 44], 2000, 120, 4);
+
+console.log(run);
