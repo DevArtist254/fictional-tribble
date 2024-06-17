086e4a00cd01dc54cc79dce117ca26f274c41261
DevArtist254 <kevinwasonga415@outlook.com>
2024-06-17 07:01:16 +0000

    init

---
+++ forkify/src/js/helpers.js
@@ -0,0 +1,27 @@
+import {TIMEOUT_SEC} from './config.js'
+
+const timeout = function (sec) {
+    return new Promise(function (_, reject) {
+        setTimeout(() => reject(new Error(`Request took too long! Timeout after ${sec} seconds`), sec * 1000));
+    })
+}
+
+export const AJAX = async function (url, uploadData = undefined) {
+    try {
+        
+        const ud = {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(uploadData)
+        }
+        const fetchPro = uploadData ? fetch(url, ud): fetch(url);
+
+        const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+        const data = await res.json();
+
+        if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
---
+++ forkify/src/js/model.js
@@ -0,0 +1,2 @@
+import {API_URL, RES_PER_PAGE, KEY} from './config.js';
+import { AJAX } from ''
