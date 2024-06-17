b3c21c43e02627a67263362e7f4a0933691715b0
DevArtist254 <kevinwasonga415@outlook.com>
2024-06-17 10:04:04 +0000

    render view id

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -1,18 +1,23 @@
 import * as model from './model.js'
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js'
 
 const controlRecipe = async function () {
     try {
-        const id = window.location.hash.slice(1);
+        //const id = window.location.hash.slice(1);
+        const id = "5ed6604591c37cdc054bc886";
         if(!id) return;
 
         recipeView.renderSpinner();
 
         await model.loadRecipe(id);
 
-        console.log(model.state);
+        recipeView.render(model.state.recipe);
     } catch (err) {
         console.error(err);
     }
 }
 
-//controlRecipe();cd 
+recipeView.addHandlerRender(controlRecipe);
+
+console.log("Hello world");
